00c57ff3d870ecadd42bf1e44861ef57b360cf74
puspita <puspitabanik213@gmail.com>
2025-08-28 22:01:44 +0600

    call history

--- script.js
+++ script.js
@@ -18,16 +18,18 @@ const moneyIcon = document.getElementById("money-icon");
 const callButtons = document.getElementsByClassName("call-btn");
 let coins = parseInt(moneyIcon.innerText);
 
+// call history
 const historyList = document.getElementById("history-list");
 const clearBtn = document.getElementById("clear-history");
 
+// call button function
 document.body.addEventListener("click", function (e) {
   let btn = e.target.closest(".call-btn");
   if (btn) {
     let card = btn.closest(".card-body");
     let serviceName = card.querySelector("p").innerText;
     let serviceNumber = card.querySelector("h1").innerText;
-    let fullServiceName = card.querySelector("h2").innerText
+    let fullServiceName = card.querySelector("h2").innerText;
 
     if (coins < 20) {
       alert("Not enough coins! You need at least 20 coins to make a call.");
@@ -38,16 +40,16 @@ document.body.addEventListener("click", function (e) {
     moneyIcon.innerText = coins;
 
     alert("Calling " + serviceName + " " + serviceNumber + "....");
-  
 
-  let now = new Date();
-  let time = now.toLocaleTimeString();
+    // call history function
+    let now = new Date();
+    let time = now.toLocaleTimeString();
 
-  let historyItem = document.createElement("div");
-  historyItem.className =
-    "flex justify-between items-center bg-gray-50 p-3 rounded-lg shadow-sm";
+    let historyItem = document.createElement("div");
+    historyItem.className =
+      "flex justify-between items-center bg-gray-50 p-3 rounded-lg shadow-sm";
 
-  historyItem.innerHTML = `
+    historyItem.innerHTML = `
     <div>
       <p class = 'font-medium'>${fullServiceName}</p>
       <p class = 'text-sm text-gray-600'>${serviceNumber}</p>
@@ -55,9 +57,33 @@ document.body.addEventListener("click", function (e) {
     <span class = 'text-xs text-gray-500'>${time}</span>
   `;
 
-  historyList.prepend(historyItem);
-}
+    historyList.append(historyItem);
+  }
 });
 clearBtn.addEventListener("click", function () {
   historyList.innerHTML = "";
+});
+
+// copy button
+
+const copyCount = document.getElementById("copy-count");
+const copyButtons = document.querySelectorAll(".copy-btn");
+
+copyButtons.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    let currentCount = parseInt(copyCount.textContent);
+    copyCount.textContent = currentCount + 1;
+
+    let card = btn.closest(".card-body");
+    let serviceNumber = card.querySelector("h1").innerText;
+
+    navigator.clipboard
+      .writeText(serviceNumber)
+      .then(function () {
+        alert("Number copied - " + serviceNumber);
+      })
+      .catch(function (err) {
+        alert("Failed to copy: " + err);
+      });
+  });
 });
